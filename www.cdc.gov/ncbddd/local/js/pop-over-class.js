c05ec1cd4743b94c88d7088e2b1c1bccbcc8bad7
The Scraper <dkastner+scraper@gmail.com>
2017-02-01 05:39:33 +0000

    2017-01-31 snapshot - chunk 53

---
+++ www.cdc.gov/ncbddd/local/js/pop-over-class.js
@@ -0,0 +1,3 @@
+$('.pop-over').popover({
+   template: '<div class="popover"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title" style="margin-top:0px;"></h3><div class="popover-content"><p></p></div></div></div>'
+}); 
